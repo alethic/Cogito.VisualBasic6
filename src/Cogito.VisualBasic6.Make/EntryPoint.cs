0d22d16a18f0108d7256594ae70cfd60cb1ac90d
Jerome Haltom <wasabi@cogito.cx>
2019-03-14 01:47:38 -0500

    Added tool for VB6 stuff. Currently silences beep.

---
+++ src/Cogito.VisualBasic6.Make/EntryPoint.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+using EasyHook;
+
+namespace Cogito.VisualBasic6.Make
+{
+
+    public class EntryPoint : IEntryPoint
+    {
+
+        public const string USER32_DLL = "user32.dll";
+        public const string ADVAPI32_DLL = "advapi32.dll";
+        public const string OLE32_DLL = "ole32.dll";
+
+        [UnmanagedFunctionPointer(CallingConvention.StdCall, CharSet = CharSet.Ansi)]
+        delegate bool DMessageBeep(uint beepType);
+
+        [DllImport(USER32_DLL, SetLastError = true)]
+        static extern bool MessageBeep(uint beepType);
+
+        [UnmanagedFunctionPointer(CallingConvention.StdCall, CharSet = CharSet.Auto)]
+        delegate int DCoInitializeEx(IntPtr pvReserved, uint dwCoInit);
+
+        [DllImport(OLE32_DLL, CharSet = CharSet.Auto, SetLastError = true, CallingConvention = CallingConvention.StdCall)]
+        static extern int CoInitializeEx([In, Optional] IntPtr pvReserved, [In] uint dwCoInit);
+
+        readonly Executor executor;
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="channelName"></param>
+        public EntryPoint(RemoteHooking.IContext context, string channelName)
+        {
+            executor = RemoteHooking.IpcConnectClient<Executor>(channelName);
+            executor.Ping();
+        }
+
+        /// <summary>
+        /// Invoked inside of the VB6 executable.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="channelName"></param>
+        public void Run(RemoteHooking.IContext context, string channelName)
+        {
+            var messageBeep = LocalHook.Create(
+                LocalHook.GetProcAddress(USER32_DLL, nameof(MessageBeep)),
+                new DMessageBeep(MessageBeepHook),
+                this);
+            messageBeep.ThreadACL.SetExclusiveACL(new[] { 0 });
+
+            var coInitializeEx = LocalHook.Create(
+                LocalHook.GetProcAddress(OLE32_DLL, nameof(CoInitializeEx)),
+                new DCoInitializeEx(CoInitializeExHook),
+                this);
+            coInitializeEx.ThreadACL.SetExclusiveACL(new[] { 0 });
+
+            RemoteHooking.WakeUpProcess();
+
+            try
+            {
+                // wait for process exit
+                while (true)
+                {
+                    Thread.Sleep(500);
+                    executor.Ping();
+                }
+            }
+            catch
+            {
+                // server failed to respond
+            }
+
+            messageBeep.Dispose();
+            LocalHook.Release();
+        }
+
+        int CoInitializeExHook(IntPtr pvReserved, uint dwCoInit)
+        {
+            var hr = CoInitializeEx(pvReserved, dwCoInit);
+            ActivateComCtx();
+            return hr;
+        }
+
+        /// <summary>
+        /// Activates the appropriate COM context on the default thread.
+        /// </summary>
+        void ActivateComCtx()
+        {
+            executor.WriteStdErr("Activating new COM ctx...\n");
+        }
+
+        /// <summary>
+        /// Silences the beeps.
+        /// </summary>
+        /// <param name="beepType"></param>
+        /// <returns></returns>
+        bool MessageBeepHook(uint beepType)
+        {
+            return true;
+        }
+
+    }
+
+}
---
+++ src/Cogito.VisualBasic6.Make/Executor.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.Remoting;
+using EasyHook;
+
+namespace Cogito.VisualBasic6.Make
+{
+
+    /// <summary>
+    /// Allows for execution of a wrapped VB6 instance.
+    /// </summary>
+    public class Executor : MarshalByRefObject, IDisposable
+    {
+
+        /// <summary>
+        /// Temporary path to a log file.
+        /// </summary>
+        readonly string log = Path.GetTempFileName();
+
+        /// <summary>
+        /// Path to the original EXE.
+        /// </summary>
+        public string Exe { get; set; }
+
+        /// <summary>
+        /// Path to the source project file to be built.
+        /// </summary>
+        public string Vbp { get; set; }
+
+        /// <summary>
+        /// Path to the output file to be built.
+        /// </summary>
+        public string Out { get; set; }
+
+        /// <summary>
+        /// Path to the output directory.
+        /// </summary>
+        public string Dir { get; set; }
+
+        /// <summary>
+        /// Additional defines.
+        /// </summary>
+        public Dictionary<string, string> Def { get; set; }
+
+        /// <summary>
+        /// Prepares for execution.
+        /// </summary>
+        void Prepare()
+        {
+            if (string.IsNullOrWhiteSpace(Exe))
+                throw new InvalidOperationException("Invalid VB6 executable.");
+            if (string.IsNullOrWhiteSpace(Vbp))
+                throw new InvalidOperationException("Invalid VB6 project file.");
+            if (string.IsNullOrWhiteSpace(Out))
+                throw new InvalidOperationException("Invalid output path.");
+
+            Exe = Exe?.Trim().TrimEnd(new[] { '/', '\\' });
+            Vbp = Vbp?.Trim().TrimEnd(new[] { '/', '\\' });
+            Out = Out?.Trim().TrimEnd(new[] { '/', '\\' });
+
+            if (File.Exists(Exe) == false)
+                throw new FileNotFoundException("Missing VB6 executable.", Exe);
+            if (File.Exists(Vbp) == false)
+                throw new FileNotFoundException("Missing VB6 project file.", Vbp);
+
+            if (string.IsNullOrWhiteSpace(Dir))
+                Dir = Environment.CurrentDirectory;
+
+            Dir = Dir.Trim().TrimEnd(new[] { '/', '\\' });
+
+            if (Def == null)
+                Def = new Dictionary<string, string>();
+        }
+
+        /// <summary>
+        /// Invoked periodically by VB6.
+        /// </summary>
+        public void Ping()
+        {
+
+        }
+
+        /// <summary>
+        /// Invoked to write a value to stderr.
+        /// </summary>
+        /// <param name="value"></param>
+        public void WriteStdErr(string value)
+        {
+            if (value != null)
+                Console.Error.Write(value);
+        }
+
+        /// <summary>
+        /// Invoked to write a value to stderr.
+        /// </summary>
+        /// <param name="value"></param>
+        public void WriteStdOut(string value)
+        {
+            if (value != null)
+                Console.Write(value);
+        }
+
+        /// <summary>
+        /// Executes VB6.
+        /// </summary>
+        public void Execute()
+        {
+            Prepare();
+
+            if (File.Exists(log))
+                File.Delete(log);
+
+            // generate new channel to call back into parent
+            string channelName = null;
+            RemoteHooking.IpcCreateServer(ref channelName, WellKnownObjectMode.Singleton, this);
+
+            // spawn VB6 process
+            RemoteHooking.CreateAndInject(
+                Exe,
+                BuildArgs(),
+                0,
+                InjectionOptions.DoNotRequireStrongName,
+                System.Reflection.Assembly.GetExecutingAssembly().Location,
+                System.Reflection.Assembly.GetExecutingAssembly().Location,
+                out var pid,
+                channelName);
+
+            // wait for exit of process
+            var prc = Process.GetProcessById(pid);
+            while (prc != null && !prc.HasExited)
+                prc.WaitForExit(100);
+
+            // copy output to stderr
+            if (File.Exists(log))
+                using (var l = new StreamReader(File.OpenRead(log)))
+                    while (l.ReadLine() is string s)
+                        Console.Error.WriteLine(s);
+        }
+
+        /// <summary>
+        /// Builds the command arguments to be executed.
+        /// </summary>
+        /// <returns></returns>
+        string BuildArgs()
+        {
+            var l = new List<string>();
+
+            l.Add("/m");
+            l.Add('"' + Vbp + '"');
+
+            l.Add("/out");
+            l.Add('"' + log + '"');
+
+            if (!string.IsNullOrWhiteSpace(Dir))
+            {
+                l.Add("/outdir");
+                l.Add('"' + Dir + '"');
+            }
+
+            if (Def.Count > 0)
+            {
+                l.Add("/D");
+                l.Add(string.Join(":", Def.Select(i => $"{i.Key}={i.Value}")));
+            }
+
+            if (!string.IsNullOrWhiteSpace(Out))
+                l.Add('"' + Out + '"');
+
+            return string.Join(" ", l);
+        }
+
+        /// <summary>
+        /// Disposes of the instance.
+        /// </summary>
+        public void Dispose()
+        {
+            try
+            {
+                if (File.Exists(log))
+                    File.Delete(log);
+            }
+            catch
+            {
+
+            }
+
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Finalizes the instance.
+        /// </summary>
+        ~Executor()
+        {
+            Dispose();
+        }
+
+    }
+
+}
