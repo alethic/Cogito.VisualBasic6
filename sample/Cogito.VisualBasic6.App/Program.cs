ab8d32dc579b1582ac90906f5089070241c3453c
Jerome Haltom <wasabi@cogito.cx>
2019-03-13 22:28:07 -0500

    More sample project stuff.

---
+++ sample/Cogito.VisualBasic6.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cogito.VisualBasic6.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ sample/Cogito.VisualBasic6.Sample/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cogito.VisualBasic6.Sample
+{
+
+    [ComVisible(true)]
+    public class Class1
+    {
+
+
+
+    }
+
+}
