e5c80e491140ddf40827b7b20ef06f2b4577125d
Jerome Haltom <jhaltom@fileandserve.com>
2019-04-30 16:27:29 -0500

    Merged.

---
+++ src/Cogito.VisualBasic6.VB6C.EasyHook/ActCtx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Cogito.VisualBasic6.VB6C.EasyHook
+{
+
+    static class ActCtx
+    {
+
+        public const uint ACTCTX_FLAG_PROCESSOR_ARCHITECTURE_VALID = 0x001;
+        public const uint ACTCTX_FLAG_LANGID_VALID = 0x002;
+        public const uint ACTCTX_FLAG_ASSEMBLY_DIRECTORY_VALID = 0x004;
+        public const uint ACTCTX_FLAG_RESOURCE_NAME_VALID = 0x008;
+        public const uint ACTCTX_FLAG_SET_PROCESS_DEFAULT = 0x010;
+        public const uint ACTCTX_FLAG_APPLICATION_NAME_VALID = 0x020;
+        public const uint ACTCTX_FLAG_HMODULE_VALID = 0x080;
+
+        public const ushort RT_MANIFEST = 24;
+        public const ushort CREATEPROCESS_MANIFEST_RESOURCE_ID = 1;
+        public const ushort ISOLATIONAWARE_MANIFEST_RESOURCE_ID = 2;
+        public const ushort ISOLATIONAWARE_NOSTATICIMPORT_MANIFEST_RESOURCE_ID = 3;
+
+
+        [StructLayout(LayoutKind.Sequential, Pack = 4, CharSet = CharSet.Unicode)]
+        public struct ACTCTX
+        {
+            public int cbSize;
+            public uint dwFlags;
+            public string lpSource;
+            public ushort wProcessorArchitecture;
+            public ushort wLangId;
+            public string lpAssemblyDirectory;
+            public IntPtr lpResourceName;
+            public string lpApplicationName;
+            public IntPtr hModule;
+        }
+
+        [DllImport("Kernel32.dll", SetLastError = true, EntryPoint = "CreateActCtxW")]
+        public static extern IntPtr CreateActCtx(ref ACTCTX actctx);
+
+        [DllImport("Kernel32.dll", SetLastError = true)]
+        public static extern bool ActivateActCtx(IntPtr hActCtx, out IntPtr lpCookie);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool DeactivateActCtx(int dwFlags, IntPtr lpCookie);
+
+        [DllImport("Kernel32.dll", SetLastError = true)]
+        public static extern bool ReleaseActCtx(IntPtr hActCtx);
+
+    }
+
+}
